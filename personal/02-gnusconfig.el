310b4aafab06c6aebd7eaa5628e40575d2010e37
Jonas Helgemo <jonas.helgemo@gmail.com>
2018-05-08 13:22:40 +0200

    feat: Add Gnus-config for work email

--- personal/02-gnusconfig.el
+++ personal/02-gnusconfig.el
@@ -1,13 +1,13 @@
-(setq gnus-use-cache t)
+;; (setq gnus-use-cache t)
 
-(setq gnus-article-date-headers '(user-defined)
-      gnus-article-time-format
-      (lambda (time)
-        (let* ((date (format-time-string "%a, %d %b %Y %T %z" time))
-               (local (article-make-date-line date 'local))
-               (combined-lapsed (article-make-date-line date
-                                                        'combined-lapsed))
-               (lapsed (progn
-                         (string-match " (.+" combined-lapsed)
-                         (match-string 0 combined-lapsed))))
-          (concat local lapsed))))
+;; (setq gnus-article-date-headers '(user-defined)
+;;       gnus-article-time-format
+;;       (lambda (time)
+;;         (let* ((date (format-time-string "%a, %d %b %Y %T %z" time))
+;;                (local (article-make-date-line date 'local))
+;;                (combined-lapsed (article-make-date-line date
+;;                                                         'combined-lapsed))
+;;                (lapsed (progn
+;;                          (string-match " (.+" combined-lapsed)
+;;                          (match-string 0 combined-lapsed))))
+;;           (concat local lapsed))))
---
+++ personal/06-gnus-webstep.el
@@ -0,0 +1,67 @@
+(setq gnus-use-cache t)
+
+(setq gnus-select-method
+      '(nnimap "gmail"
+	       (nnimap-address "imap.gmail.com")
+	       (nnimap-server-port 993)
+	       (nnimap-stream ssl)
+               (nnir-search-engine imap)))
+
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587
+				   "jonas.helgemo@webstep.no" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+
+(setq user-full-name "Jonas Helgemo"
+      user-mail-address "jonas.helgemo@webstep.no")
+
+;; http://groups.google.com/group/gnu.emacs.gnus/browse_thread/thread/a673a74356e7141f
+(when window-system
+  (setq gnus-sum-thread-tree-indent "  ")
+  (setq gnus-sum-thread-tree-root "") ;; "● ")
+  (setq gnus-sum-thread-tree-false-root "") ;; "◯ ")
+  (setq gnus-sum-thread-tree-single-indent "") ;; "◎ ")
+  (setq gnus-sum-thread-tree-vertical        "│")
+  (setq gnus-sum-thread-tree-leaf-with-other "├─► ")
+  (setq gnus-sum-thread-tree-single-leaf     "╰─► "))
+(setq gnus-summary-line-format
+      (concat
+       "%0{%U%R%z%}"
+       "%3{│%}" "%1{%d%}" "%3{│%}" ;; date
+       "  "
+       "%4{%-20,20f%}"               ;; name
+       "  "
+       "%3{│%}"
+       " "
+       "%1{%B%}"
+       "%s\n"))
+(setq gnus-summary-display-arrow t)
+
+(setq gnus-parameters
+      '((".*"
+         (display . all)
+         (gnus-use-scoring nil))))
+(setq gnus-permanently-visible-groups "INBOX")
+
+(setq gnus-thread-sort-functions
+      '(gnus-thread-sort-by-most-recent-number))
+
+(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
+
+(setq gnus-activate-level 1)
+
+(setq mm-verify-option 'always)
+(setq mm-decrypt-option 'always)
+(setq mm-encrypt-option 'guided)
+(setq gnus-buttonized-mime-types '("multipart/signed"))
+
+(setq gnus-interactive-exit nil)
+(add-hook 'kill-emacs-hook (lambda ()
+                             (when (boundp 'gnus-group-exit)
+                               (gnus-group-exit))))
+
+(setq gnus-use-full-window nil)
